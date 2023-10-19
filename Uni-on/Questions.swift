bed92eb85825d3d74a82bbb0f1f85bda69697978
Jakub Mantelli <jakubmantelli@gmail.com>
2023-10-19 11:49:57 +0200

    Union
    Questions and NHIE content view

---
+++ Uni-on/Questions.swift
@@ -0,0 +1,27 @@
+//
+//  Questions.swift
+//  neverhaveiever
+//
+//  Created by Jakub Mantelli on 18/10/23.
+//
+
+import Foundation
+
+
+struct Question {
+    
+    
+    
+    let category: String
+    let text: String
+    
+}
+
+var questions = [
+    Question(category: "Hobby & Interests", text: "Never have I ever tried an unconventional hobby just for the thrill of it."),
+    Question(category: "Movies & TV Shows", text: "Never have I ever watched a horror movie."),
+    Question(category: "Special Owl Prompt", text: "owl task"),
+    Question(category: "Adventure & Thrills", text: "Never have I ever gone skydiving."),
+    Question(category: "Relationships & Romance:", text: "Never have I ever had a blind date.")
+]
+
