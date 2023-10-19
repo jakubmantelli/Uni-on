28a685850c4f5865412a7887437ea428f5fccbf9
Jakub Mantelli <jakubmantelli@gmail.com>
2023-10-19 11:36:18 +0200

    Initial Commit

---
+++ Uni-on/Uni_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uni_onApp.swift
+//  Uni-on
+//
+//  Created by Jakub Mantelli on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Uni_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
