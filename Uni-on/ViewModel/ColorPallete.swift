22921d3d604337b05a545c2e15b38e4e05a08210
Mohammad Solki <53335722+msolki@users.noreply.github.com>
2023-10-20 11:26:07 +0200

    Added the ColorPallete - Updated design pattern

---
+++ Uni-on/ViewModel/ColorPallete.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Uni-on
+//
+//  Created by Mohammad Solki on 20/10/23.
+//
+
+import SwiftUI
+
+struct ColorPallete {
+  static let primary: Color = Color(red: 0.57, green: 0.63, blue: 0.89)
+  static let primaryLight: Color = Color(red: 0.92, green: 0.94, blue: 1)
+  static let primaryDark: Color = Color(red: 0.38, green: 0.47, blue: 0.87)
+  static let secondary: Color = Color(red: 0.82, green: 0.89, blue: 0.24)
+  static let secondaryLight: Color = Color(red: 0.93, green: 0.97, blue: 0.75)
+  static let third: Color = Color(red: 0.95, green: 0.42, blue: 0.55)
+  static let thirdLight: Color = Color(red: 0.98, green: 0.87, blue: 0.89)
+
+}
