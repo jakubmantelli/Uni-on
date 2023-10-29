6dc026cb2b25e3e48c3fba1cd3a808ef4276d2cc
mattia <mattiadefranco24@gmail.com>
2023-10-29 17:54:10 +0100

    Add Persona Model for each personality all the variable, but don't how to recall them in other files, Persona VIew commented for not have bugs

---
+++ Uni-on/View/PersonaViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaViewModel.swift
+//  Uni-on
+//
+//  Created by Mattia De Franco on 27/10/23.
+//
+
+
+class PersonaViewModel {
+    
+    var Persona1 = [
+        Persona(description2: "Wise Night Owl 🦉", imageName: "OwlFullp"),
+        Persona(description2: "Vibrant peacock 🦚", imageName: "Peacock_Transparent"),
+        Persona(description2: "Compassionate Dolphin 🐬", imageName: ""),
+        Persona(description2: "Quick Quokka 🐹", imageName: "Quokka_Transparent")
+    ]
+}
